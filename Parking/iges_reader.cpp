2ce8b8ac99c382866e662d1ebe19c8c58d4c486e
Gneogeo <gneogeo@gmail.com>
2012-12-08 09:01:24 +0200

    Starting to support IGES

---
+++ Parking/iges_reader.cpp
@@ -0,0 +1,202 @@
+#include "iges_reader.h"
+
+
+#include <stdlib.h>
+#include <math.h>
+#include "geometry.h"
+#include "coord_system.h"
+
+#include <stdio.h>
+#include <qdebug.h>
+
+
+static void stripTrailingSpaces(char *d)
+{
+	if (!d) return;
+	int n=strlen(d);
+	for (;n>0; n--) {
+		if (d[n-1]!=' ') {
+			d[n]=0;
+			break;
+		}
+	}
+}
+
+class IGES_line {
+	public:
+	char data[73];
+	char letterCode;
+	int sequenceNumber;
+
+	int readLine(FILE *fp) {
+		if (feof(fp)) return 0;
+		char temp[100];
+		fgets(temp,100,fp);
+		memcpy(data,temp,72*sizeof(char));
+		data[72]=0;
+
+		letterCode=temp[72];
+		sequenceNumber=atoi(&temp[73]);
+		return 1;
+	}
+
+};
+
+static int readHollerithString(const char *data,char *target)
+{
+	int ret=0;
+	int total=0;
+	while (data[0]!='H') {
+		if (data[0]=='\0') return ret;
+		total=total*10+ data[0]-'0';
+		data++; ret++;
+	}
+	data++; ret++;
+	int k;
+	for (k=0; k<total; k++) {
+		target[k]=data[0];
+		data++; ret++;
+	}
+	target[total]=0;
+	return ret;
+}
+
+static int readDelimiterString(const char *data,char delim,char delim1,char *target)
+{
+	int k=0;
+	while (data[0]!=delim && data[0]!=delim1) {
+		target[k]=data[0];
+		k++;
+		data++;
+		if (data[0]==0) break;
+	}
+	target[k]=0;
+	return k;	
+}
+
+typedef struct IGES_directory {
+	int entityType;
+	int parameterData;
+	int structure;
+	int lineFontPattern;
+	int level;
+	int view;
+	int transMatrix;
+	int labelDisplayAssoc;
+	int statusNumber;
+	int sequenceNumber;
+	int lineWeightNumber;
+	int colorNumber;
+	int paramLineCount;
+	int formNumber;
+	char entityLabel[9];
+	int entitySubscriptNumber;
+	int seqNumber;
+}IGES_directory;
+
+void readIGES(Geometry *geom,const char *name)
+{
+
+	FILE *fp=fopen(name,"rb");
+	if (!fp) return;
+
+	IGES_line igs;
+
+	int ok=igs.readLine(fp);
+
+	while (ok) {
+		if (igs.letterCode!='S') break;
+		ok=igs.readLine(fp);
+	}
+
+	int globalParamCount=0;
+	char ParameterDelimiterChar=',';
+	char RecordDelimiterChar=';';
+	char GlobalParam[26][100];
+
+
+	while (ok) {
+		if (igs.letterCode!='G') break;
+		
+		stripTrailingSpaces(igs.data);
+		char *pnt=igs.data;
+		if (globalParamCount==0) {
+			if (pnt[0]==',' && pnt[1]==',') {
+				globalParamCount+=2;
+				pnt+=2;
+			} else if (pnt[0]=='1' && pnt[1]=='H' && pnt[2]==pnt[3] && pnt[3] !=pnt[4] && pnt[4]=='1' && pnt[5]=='H') {
+				ParameterDelimiterChar=pnt[2];
+				RecordDelimiterChar=pnt[6];
+				pnt=&pnt[8];
+				globalParamCount+=2;
+			} else if (pnt[0]=='1' && pnt[1]=='H' && pnt[2]==pnt[3] && pnt[3]==pnt[4]) {
+				ParameterDelimiterChar=pnt[2];
+				pnt=&pnt[5];
+				globalParamCount+=2;
+			} else if (pnt[0]==',' && pnt[1]=='1' && pnt[2]=='H' && pnt[4]==',') {
+				RecordDelimiterChar=pnt[3];
+				pnt=&pnt[5];
+				globalParamCount+=2;
+			}
+		}
+		while (pnt[0]!=0) {
+			switch (globalParamCount) {
+				case 2: case 3: case 4: case 5:
+				case 11:
+				case 14:
+				case 17:
+				case 20: case 21:
+				case 24: case 25:
+					pnt+=readHollerithString(pnt,GlobalParam[globalParamCount]);
+					if (pnt[0]!=0) {
+						pnt++;
+					}
+					break;
+				default:
+					pnt+=readDelimiterString(pnt,ParameterDelimiterChar,RecordDelimiterChar,GlobalParam[globalParamCount]);
+					if (pnt[0]!=0) {
+						pnt++;
+					}
+					break;
+
+			}
+			qDebug("Parameter %d is '%s'",globalParamCount,GlobalParam[globalParamCount]);
+			globalParamCount++;
+		}
+
+
+		ok=igs.readLine(fp);
+	}
+
+	while (ok) {
+		if (igs.letterCode!='D') break;
+		char Directory[18][9];
+		char *pnt=igs.data;
+		int k;
+		for (k=0; k<9; k++) {
+			memcpy(Directory[k],pnt,8);
+			Directory[k][8]=0;
+			pnt+=8;
+		}
+
+		ok=igs.readLine(fp);
+		if (!ok) break;
+		if (igs.letterCode!='D') break;
+
+		for (k=10; k<18; k++) {
+			memcpy(Directory[k],pnt,8);
+			Directory[k][8]=0;
+			pnt+=8;
+		}
+
+
+		
+		ok=igs.readLine(fp);
+
+	}
+
+
+	/*TODO*/
+
+	fclose(fp);
+}
---
+++ Parking/iges_reader.h
@@ -0,0 +1,8 @@
+#ifndef IGES_READER_H
+#define IGES_READER_H
+
+class Geometry;
+
+void readIGES(Geometry *geom,const char *name);
+
+#endif
